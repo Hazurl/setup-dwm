0543e132326d24f9cb6bd80c93cadcc739c6d8bc
Hazurl <ricoujules@gmailcom>
2023-10-22 20:36:59 +0200

    Config: Tweak font size and add icon for kitty

--- config.h
+++ config.h
@@ -12,14 +12,14 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int force_bar_height   = 24;
+static const int force_bar_height   = 36;
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int max_tag_name_width = 400;
 static const int trianglewidth			= force_bar_height / 4;
-static const char *fonts[] = {"Hack:style=Regular:size=10", "Fira Mono:style=Regular:size=10", "Symbols Nerd Font:size=12"};
+static const char *fonts[] = {"Hack:style=Regular:size=14", "Fira Mono:style=Regular:size=14", "Symbols Nerd Font:size=16"};
 #define QUOTED_(x) #x
 #define QUOTED(x) QUOTED_(x)
-#define dmenufont "Hack:style=Regular:size=10"
+#define dmenufont "Hack:style=Regular:size=14"
 
 static const char col_themed_Background[] = "#282a36";
 static const char col_themed_CurrentLine[] = "#44475a";
@@ -46,7 +46,7 @@ static const AlternateTagName alternate_tag_names[] = {
 	{ "spotify", "" },
 	{ "google-chrome", "" },
 	{ "code", "" },
-	{ "termite", "" },
+	{ "kitty", "" },
 	{ "microsoft teams - preview", "" },
 	{ "slack", "" }
 };
